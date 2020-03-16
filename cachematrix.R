d7f361d4ea7f1cf5ea2e409f4d8338b25152eded
yd8012 <61741673+yd8012@users.noreply.github.com>
2020-03-17 03:15:36 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implemented inverse-cacheable matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## make a custom inverse-cacheable matrix
+## Parameter
+## x : matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Given x should be matrix
+        ## Also, it should be square.
+        if (!inherits(x, "matrix") | dim(x)[1] != dim(x)[2]) {
+                stop("makeCacheMatrix must be provided with the square matrix")
+        }
 
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+            setinv = setinv,
+            getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve
+## Return inverse of the matrix.
+## If cache exists, return the cache value.
+## Else, do the calculation - save it to the cache and return.
+## Parameter
+## x : custom cachable-matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("Getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setinv(i)
+        i
 }
